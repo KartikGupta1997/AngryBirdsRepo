7a7c1b35f19da4aa783caf759312caa394e24d0d
Kartik Gupta <kartikg1997@gmail.com>
2018-07-08 20:24:42 +0530

    Added basic Controller script to bird
    Added basic force to bird so it flies with 8f, 8f,0 force

---
+++ Assets/Scripts/birdController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdController : MonoBehaviour {
+
+    public Rigidbody rb;
+    [SerializeField] private float xval, yval;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rb.AddForce(xval,yval,0, ForceMode.Impulse);
+
+        }
+    }
+
+}
