e9243452d4f77d99d8d5f5aeb5d821a722c89da4
Kartik Gupta <kartikg1997@gmail.com>
2018-07-09 00:23:17 +0530

    Added script to pig
    Added setActive to pig script. If pig collides more than or equal to maxhits, it will dissappear

---
+++ Assets/Scripts/pigController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pigController : MonoBehaviour {
+    [SerializeField] private int maxHits;
+    private int hits;
+
+    
+    void Start()
+    {
+        hits = 0;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits++;
+        if (hits >= maxHits) {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
