ceb4092b0e5906574a313b812ba0c9ffc07b5471
Kartik Gupta <kartikg1997@gmail.com>
2018-07-14 01:13:27 +0530

    Displaying power in UI text
    each bird when spawns, finds the text ui element to show the power

--- Assets/Scripts/birdController.cs
+++ Assets/Scripts/birdController.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class birdController : MonoBehaviour {
 
     public Rigidbody rb;
-    [Range(10,16)][SerializeField]private float xPow;
+    private float xPow;
     public int scoreTotal;
     public bool canJump;
+    public int destroyTime;
+    public Text powerText;
     
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         canJump = true;
-       
+        xPow = 10f;
+        powerText = GameObject.Find("Canvas/powerText").GetComponent<Text>(); //Finds the textUI element in 
+        powerTextFunc();
+
     }
+    // Update is called once per frame
     private void Update()
     {
+
+        powerTextFunc();
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Rotate(new Vector3(0, 0, 5));
@@ -26,19 +35,35 @@ public class birdController : MonoBehaviour {
         {
             transform.Rotate(new Vector3(0, 0, -5));
         }
-        Debug.Log("Score is: " + scoreTotal);
 
+
+
+        if (Input.GetKeyDown(KeyCode.W) && xPow<=15) //Mathf.Clamp not working here 
+        {
+            xPow++;
+            Debug.Log(xPow);
+
+        }
+        else
+        if (Input.GetKeyDown(KeyCode.S) && xPow >= 10) //Mathf.Clamp not working here 
+        {
+            xPow--;
+            Debug.Log(xPow);
+        }
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
         {
+            
             this.rb.AddRelativeForce(new Vector3(xPow, 0,0), ForceMode.Impulse);
-            Destroy(gameObject, 3); //Destroys gameobject after 3 seconds
+            Destroy(gameObject, destroyTime); //Destroys gameobject after "destroyTime" seconds
             canJump = false;
         }
     }
-
-    
+    void powerTextFunc() // For displaying power value
+    {
+        powerText.text = "Power: " + xPow.ToString();
+    }
 }
