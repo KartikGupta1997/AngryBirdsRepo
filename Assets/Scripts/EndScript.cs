7823f1ea1b1f1f710ae3d5a237546353d2f60f2e
Kartik Gupta <kartikg1997@gmail.com>
2018-07-17 21:45:36 +0530

    Added play again button

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class TestScript : MonoBehaviour {
+public class EndScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        transform.Rotate(new Vector3(0, 0, 0));
-    }
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-
+		
+	}
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
